1bf9085f51379310b5b894714ecaddc6115400f5
Chris Snow <chsnow123@gmail.com>
2015-08-27 01:21:39 +0000

    enable debugging

--- run_sync.sh
+++ run_sync.sh
@@ -17,11 +17,11 @@ tmux split-window -t 'run_sync'
 
 tmux send -t 'run_sync.0' "cd /vagrant/sync-android-p2p";
 tmux send -t 'run_sync.0' C-m;
-tmux send -t 'run_sync.0' "./gradlew execute -DDB_DIR=/tmp/datastores/";
+tmux send -t 'run_sync.0' "GRADLE_OPTS='-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5006' ./gradlew execute -DDB_DIR=/tmp/datastores/";
 tmux send -t 'run_sync.0' C-m;
 
 
-tmux send -t 'run_sync.1' "sleep 10";
+tmux send -t 'run_sync.1' "sleep 30";
 tmux send -t 'run_sync.1' C-m;
 tmux send -t 'run_sync.1' "curl -X POST -H 'Content-Type:application/json' -d ' { \"source\": \"http://localhost:5984/newdb\", \"target\": \"http://localhost:8182/mydb\" }' http://localhost:5984/_replicate";
 tmux send -t 'run_sync.1' C-m;
