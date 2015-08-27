bbde35cccce5abeec65a29213386cfb86cd3a6de
Chris Snow <chsnow123@gmail.com>
2015-08-27 00:38:40 +0000

    add utilities

--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install git couchdb
+sudo apt-get install git couchdb tmux sqlite3 wireshark -y
 
 cd /vagrant
 git clone https://github.com/cloudant/sync-android
@@ -13,5 +13,3 @@ git clone https://github.com/snowch/sync-android-p2p
 cd sync-android-p2p/
 git checkout migrate_to_cloudant_sync_0_10_0
 
-mkdir -p /tmp/datastores
-./gradlew execute -DDB_DIR=/tmp/datastores/
