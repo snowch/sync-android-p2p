8011d9dfe366960e53a1e8e97cb22bcb7f5c0d9b
Chris Snow <chsnow123@gmail.com>
2015-08-27 02:13:22 +0000

    update vagrant setup

--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-sudo apt-get install git couchdb tmux sqlite3 wireshark -y
+sudo apt-get update
+sudo apt-get remove openjdk-6* -y
+sudo apt-get install git couchdb tmux sqlite3 wireshark openjdk-7-jdk -y
+
+wget -P /home/vagrant/ "https://eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/mars/R/eclipse-java-mars-R-linux-gtk.tar.gz&r=1" -O eclipse-java-mars-R-linux-gtk.tar.gz
+tar xzvf eclipse-java-mars-R-linux-gtk.tar.gz
+
+sudo apt-get install linux-image-generic-lts-trusty -y
+
+
 
 cd /vagrant
 git clone https://github.com/cloudant/sync-android
@@ -12,4 +21,5 @@ cd /vagrant
 git clone https://github.com/snowch/sync-android-p2p
 cd sync-android-p2p/
 git checkout migrate_to_cloudant_sync_0_10_0
+./gradlew ecilpse
 
