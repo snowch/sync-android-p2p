5cc647ca75d23b4bac6409ea0a073fd3b71533fc
christopher snow <chsnow123@gmail.com>
2015-11-23 11:53:48 +0000

    add missing files

---
+++ src/test/java/P2PTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.fail;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.cloudant.sync.datastore.Datastore;
+import com.cloudant.sync.datastore.DatastoreManager;
+import com.cloudant.sync.datastore.DocumentBodyFactory;
+import com.cloudant.sync.datastore.MutableDocumentRevision;
+import com.cloudant.sync.replication.Replicator;
+import com.cloudant.sync.replication.ReplicatorBuilder;
+
+
+public class P2PTest extends P2PAbstractTest {
+
+	
+	@Before
+	public void setup() throws Exception {
+		createServer(8182);
+		createServer(8184);
+	}
+	
+	@Test
+	public void test() throws Exception {
+
+		URI uri = new URI("http://localhost:8183/mydb");
+
+		DatastoreManager manager = new DatastoreManager(databaseDirs.get(8182));
+		Datastore ds = manager.openDatastore("mydb");
+
+
+		MutableDocumentRevision rev = new MutableDocumentRevision();
+		Map<String, Object> json = new HashMap<String, Object>();
+		json.put("description", "Buy milk");
+		json.put("completed", false);
+		json.put("type", "com.cloudant.sync.example.task");
+		rev.body = DocumentBodyFactory.create(json);
+		ds.createDocumentFromRevision(rev);
+
+		Replicator replicator = ReplicatorBuilder.push().from(ds).to(uri).build();
+		waitForReplication(replicator);
+
+		fail("Not yet implemented");
+	}
+
+}
